59d0106d8c9e6355999f74b905ef614b079b7189
Osha G <ogroetz@gmail.com>
2020-04-17 17:24:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+
+def prime?(n)
+  prime_n = (2..n-1).to_a 
+  if n < 2 
+    return false
+  else prime_n.each do |integer|
+    if n % integer == 0 
+      return false
+    end
+  end 
+  end
+  return true
+end
+  
